fef3b8e943c84a3d1264978fea831b81432ddc69
SergeyDarnopykh <sergey.darnopyh@gmail.com>
2017-11-20 06:47:39 +0300

    Fix Chess-board task

--- Program Structure/Chess-Board.js
+++ Program Structure/Chess-Board.js
@@ -1,20 +1,21 @@
-function makeBoard(size) {
+function makeBoard(width, height) {
     let board = '';
 
-    for  (let i = 0, multiplicity = 0; i < size * size; i++) {
-        if  (i % size == 0) {
-            board += '\n';
-            multiplicity = !multiplicity;
-        }
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if ((i + j) & 1) {
+                board += ' ';
+            } else {
+                board += '#';
+            }
 
-        if  ((i & 1) == multiplicity) {
-            board += '#';
-        } else {
-            board += ' ';
+            if (j == width - 1) {
+                board += '\n';
+            }
         }
     }
 
     return board;
 }
 
-console.log( makeBoard(8) );
+console.log( makeBoard(3, 5) );
