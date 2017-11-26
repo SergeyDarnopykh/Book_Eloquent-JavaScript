da080609bab36c9657c8707c1fcdbf8e8e001a3a
SergeyDarnopykh <sergey.darnopyh@gmail.com>
2017-11-26 17:01:48 +0300

    Add 1 task to Higher-Order Functions

--- Higher-Order Functions/Historical Life Expectancy.js
+++ Higher-Order Functions/Historical Life Expectancy.js
@@ -5,10 +5,45 @@ function average(array) {
 }
 
 // My code
-
-
-groupBy(array, test) {
-    array.forEach( (item) => {
-        if (test(item))
-    } );
+function calculateCentury(person) {
+    return Math.ceil(person.died / 100);
 }
+
+function calculateAge(person) {
+    return person.died - person.born;
+}
+
+function groupBy(array, computeGroup) {    
+    // Keys : centuries 
+    // Data: arrays of age
+    const group = {};
+
+    array.forEach( (person) => {
+        const key = computeGroup(person);
+
+        if (!group[key]) {
+            group[key] = [];
+        }
+
+        group[key].push(person);
+    } );
+
+    return group;
+}
+
+function calculateAverage(object) {
+    for (key in object) {
+        const ages = object[key]. map(calculateAge);
+        object[key] = average(ages);
+    }
+}
+
+function showObject(object) {
+    for (key in object) {
+        console.log(`${key}: ${object[key]}`);
+    }
+}
+
+const peopleAgeByCentury = groupBy(ancestry, calculateCentury);
+calculateAverage(peopleAgeByCentury)
+showObject(peopleAgeByCentury);
